77142485c69519f09e7e7dacbd85136cdbdc1da4
Wojciech Łaciak <skonry1604@gmail.com>
2022-05-27 22:58:28 +0200

    initial commit

---
+++ src/test/java/com/investingsimulator/InvestingSimulatorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.investingsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestingSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
